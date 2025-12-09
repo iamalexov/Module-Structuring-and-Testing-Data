89a56fe8e9e033c17d8bfb39f0b5d8e9261576b0
Alex <oleksi.ovsiannikov@gmail.com>
2025-12-09 23:43:08 +0100

    init: sprint2/1-key-errors/0js

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,27 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> The function should take a string, make the first letter uppercase, and then add the rest of the string starting from the second character.
+// To do this, should use the slice() function
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
 
-// =============> write your explanation here
-// =============> write your new code here
+  /*function capitalise(str) {
+  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str; */
+
+
+
+
+// =============> The error occurs because the variable str is declared twice
+// JS does not allow redeclaring the same variable  name in the same scope. To fix this,
+// we can either use a different variable name or assign directly to str instead of redeclaring it
+
+// =============> 
+  function capitalise(str) {
+   str = `${str[0].toUpperCase()}${str.slice(1)}`;
+return str;
+
+}   
+
+console.log(capitalise("alex"));
