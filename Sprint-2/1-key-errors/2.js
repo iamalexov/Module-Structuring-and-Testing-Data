d37f6122163cc1a8bc5948aead92ad83573eae58
Alex <oleksi.ovsiannikov@gmail.com>
2025-12-11 00:43:52 +0100

    feat:completed 1-key-errors/2js

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,32 @@
 
 // Predict and explain first BEFORE you run any code...
+// answer:
+// This function takes a number as an argument and returns the square of that number.
+// It does this by multiplying the argument num by itself (num * num)
+
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> the f-n will be syntax error; F-n parameters must be variable names, not numbers
 
-function square(3) {
+
+/* function square(3) {
     return num * num;
-}
+} */
 
-// =============> write the error message here
+// =============> write the error message here: 
+// VM541:1 Uncaught SyntaxError: Unexpected number
 
-// =============> explain this error message here
+// =============> explain this error message here:
+// Means that JavaScript found a number in a place where a number is not allowed
+// So JS says: 'I don’t expect to see a number here' and throws a syntax error
 
 // Finally, correct the code to fix the problem
 
-// =============> write your new code here
+// =============> write your new code here:
+function square(num) {
+    return num * num;
+}
+console.log(square(3))
 
 
