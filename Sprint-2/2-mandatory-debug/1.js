a9546619cb77784092b7daef84045861559463c1
Alex <oleksi.ovsiannikov@gmail.com>
2025-12-11 01:41:03 +0100

    feat: completed 2-mandatory-debug/1.js

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,24 @@
 // Predict and explain first...
-//  =============> write your prediction here
+// The function is supposed to return the sum of two numbers, but it does not work correctly because the return statement is used improperly.
 
-function sum(a, b) {
+
+
+/* function sum(a, b) {
   return;
   a + b;
 }
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`); */
 
-// =============> write your explanation here
+
+
+// There is semicolon right after return, so the function returns immediately and the expression a + b is never executed
+// The sum of 10 and 32 is undefined
+
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
