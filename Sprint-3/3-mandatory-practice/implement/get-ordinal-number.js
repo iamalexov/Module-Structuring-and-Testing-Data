5b547cea3093cf84fcba66c3dc6475b03138e25b
Alex <oleksi.ovsiannikov@gmail.com>
2025-12-21 01:51:37 +0100

    feat: complete sprint-3/3-mandatory7get-ordinal-number

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,17 @@
 function getOrdinalNumber(num) {
- return "1st";
+    if (num % 100 >= 11 && num % 100 <= 13) {
+        return num + "th";
+}
+switch(num % 10) {
+    case 1:
+        return num + "st";
+    case 2:
+        return num + "nd";
+    case 3:
+        return num + "rd";
+    default:
+        return num + "th";
+}
 }
 
 module.exports = getOrdinalNumber;
